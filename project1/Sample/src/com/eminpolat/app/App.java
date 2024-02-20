f44c8865f75c1fe934a87a35e37b472087b790e8
Emin Polat <eminpolat749@gmail.com>
2024-02-20 21:23:52 +0300

    second commit

---
+++ project1/Sample/src/com/eminpolat/app/App.java
@@ -0,0 +1,15 @@
+package com.eminpolat.app;
+
+class App {
+    public static void main(String[] args)
+    {
+        var x = 15;
+
+    }
+}
+
+class A {
+    public static class B {
+        
+    }
+}
